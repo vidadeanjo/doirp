bfe47daf34901f95a599cf3e1a47b31088cc57db
unknown <chinhalamesack@gmail.com>
2025-05-28 21:30:10 +0100

    setimo commit - npm run build

--- resources/views/livewire/priod.blade.php
+++ resources/views/livewire/priod.blade.php
@@ -4,12 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PRIOD</title>
-    <link rel="icon" href="{{ asset('build/assets/imgs/LOGO PRIOD.png')}}" type="image/png">
-    <link rel="stylesheet" href="{{ asset('build/assets/css/owl.theme.default.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('build/assets/css/animate.css')}}">
-    <link rel="stylesheet" href="{{ asset('build/assets/css/bootstrap.min.css')}}">
-    
-  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
        <!-- CSS do AOS -->
