a52096e6965eb8c12b6830d09c690c9263b4dc86
unknown <chinhalamesack@gmail.com>
2025-05-29 06:43:11 +0100

    decimo e cinco commit - styles css in assets

--- resources/views/livewire/priod.blade.php
+++ resources/views/livewire/priod.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="{{ asset('build/assets/css/owl.theme.default.min.css')}}">
     <link rel="stylesheet" href="{{ asset('build/assets/css/animate.css')}}">
     <link rel="stylesheet" href="{{ asset('build/assets/css/bootstrap.min.css')}}">
-    
+    <link rel="stylesheet" href="{{ asset('build/assets/css/styles.css')}}">
   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
